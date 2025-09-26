ee103131924440c2f4c91d5d9df25252a96e06fd
GroothuijsenTjeu <groothuijsentjeu@gmail.com>
2025-09-26 08:31:26 +0200

    Add week 3 page

--- Openscript.R
+++ Openscript.R
@@ -38,4 +38,85 @@ require(sna)
 ?sna
 triad.census(net4s)
 
+#webscrape test
+fpackage.check <- function(packages) {
+  lapply(packages, FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  })
+}
+
+fsave <- function(x, file = NULL, location = "./data/processed/") {
+  ifelse(!dir.exists("data"), dir.create("data"), FALSE)
+  ifelse(!dir.exists("data/processed"), dir.create("data/processed"), FALSE)
+  if (is.null(file))
+    file = deparse(substitute(x))
+  datename <- substr(gsub("[:-]", "", Sys.time()), 1, 8)
+  totalname <- paste(location, datename, file, ".rda", sep = "")
+  save(x, file = totalname)  #need to fix if file is reloaded as input name, not as x. 
+}
+
+fload <- function(filename) {
+  load(filename)
+  get(ls()[ls() != "filename"])
+}
+
+fshowdf <- function(x, ...) {
+  knitr::kable(x, digits = 2, "html", ...) %>%
+    kableExtra::kable_styling(bootstrap_options = c("striped", "hover")) %>%
+    kableExtra::scroll_box(width = "100%", height = "300px")
+}
+
+install.packages("rvest")
+require(rvest)
+install.packages("jsonlite")
+require(jsonlite)
+require(dplyr)
+jt <- read_html("https://api.openalex.org/authors?search=Vincent+Buskens") %>%
+  html_text2()
+jt_json <- fromJSON("https://api.openalex.org/authors?search=Vincent+Buskens", simplifyVector = FALSE)
+glimpse(jt_json, max.level = 1)
+
+jt_json$results$affiliations
+
+
+
+#Scraping member info from the ICS website
+require(rvest)
+require(xml2)
+require(dplyr)
+
+#Faculty
+icsfac<-read_html("https://ics-graduateschool.nl/faculty/")
+icsfac<-icsfac %>%
+  html_nodes("body") %>%
+  xml_find_all("//tbody") %>%
+  html_table()
+icsfac<-bind_rows(icsfac)
+icsfac$X5<-"Faculty"
+
+#Postdocs
+icspost<-read_html("https://ics-graduateschool.nl/postdocs/")
+icspost<-icspost %>%
+  html_nodes("body") %>%
+  xml_find_all("//tbody") %>%
+  html_table()
+icspost<-bind_rows(icspost)
+icspost$X5<-"Postdoc"
+
+#PhDs
+icsphd<-read_html("https://ics-graduateschool.nl/phds/")
+icsphd<-icsphd %>%
+  html_nodes("body") %>%
+  xml_find_all("//tbody") %>%
+  html_table()
+icsphd<-bind_rows(icsphd)
+icsphd$X5<-"PhD"
+
+#Merging into one table
+ics<-rbind(icsfac,icspost)
+ics<-rbind(ics,icsphd)
+ics
 
